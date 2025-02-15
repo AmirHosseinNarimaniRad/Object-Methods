b039b3278de460085829521f4efdd84993e037c9
AmirHosseinNarimaniRad <Amir.hossein.narimani.rad@gmail.com>
2025-02-15 19:12:21 +0330

    Object
    for test

---
+++ Object Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Student1 = new Student("Jim", "Business", 2.8);
+            Student Student2 = new Student("Pam", "Art", 3.6);
+            Student student3 = new Student("Oscar", "Accounting", 3.2);
+
+            Console.WriteLine(Student1.HasHonors());
+            Console.WriteLine(Student2.HasHonors());
+            Console.WriteLine(student3.HasHonors());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Object Methods/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Methods
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string major { get; set; }
+        public double gpa { get; set; }
+
+
+        //a Constructor to create a student object
+        public Student(string aName, string aMajor, double aGpa)
+        {
+            Name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+
+        public bool HasHonors()
+        {
+            if (gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
